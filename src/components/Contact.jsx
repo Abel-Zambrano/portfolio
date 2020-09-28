d831b2ee6395402d4fb29103ad5771b37ecb17ce
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-09-27 21:56:29 -0700

    added link to email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,8 +16,10 @@ function Contact() {
                 </div>
             </div>
             <div>
-                <h3 className='contact'>Phone: 510-619-4434</h3>
-                <h3 className='contact'>Email: azambrano1417@gmail.com</h3>
+                <h3 className='contact'>Phone: (510) 619-4434</h3>
+                <a href="mailto:azambrano@gmail.com">
+                    <h3 id='email' className='contact'>Email: azambrano1417@gmail.com</h3>
+                </a>
             </div>
         </div>
     )
