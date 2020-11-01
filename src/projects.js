0d7aec2d70cc6acf5c3cf5f2c2e2e22783cd8dea
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-01 11:16:24 -0800

    update

--- src/projects.js
+++ src/projects.js
@@ -1,14 +1,22 @@
 const projects = [
     {
         id: 1,
-        img: 'https://live.staticflickr.com/65535/50391646853_f348ed9fdb_o.png',
-        name: 'Airbnb Magic Clone Page',
-        url: 'https://airbnb-magic.surge.sh/',
-        tech: 'HTML5, CSS3',
-        git: 'https://github.com/Abel-Zambrano/airbnb'
+        img: 'https://live.staticflickr.com/65535/50546368751_3981d6859e_b.jpg',
+        name: 'Pokemon Card',
+        url: 'https://tarty-paint.surge.sh/',
+        tech: 'React, HTML5, CSS3, JavaScript, AJAX, APIs',
+        git: 'https://github.com/Abel-Zambrano/pokemon-api'
     },
     {
         id: 2,
+        img: 'https://live.staticflickr.com/65535/50555307523_55b69dff0d_b.jpg',
+        name: 'Digital Flix',
+        url: '',
+        tech: 'React, HTML5, CSS3, JavaScript, AJAX, APIs',
+        git: 'https://github.com/Abel-Zambrano/digital-flix'
+    },
+    {
+        id: 3,
         img: 'https://live.staticflickr.com/65535/50392526587_56dc91d577_o.png',
         name: 'Web Blog',
         url: 'https://sleepy-lowlands-55100.herokuapp.com/',
@@ -17,7 +25,7 @@ const projects = [
 
     },
     {
-        id: 3,
+        id: 4,
         img: 'https://live.staticflickr.com/65535/50392371011_9b2f33856e_o.png',
         name: 'Cat Gallery',
         url: 'https://polar-spire-96585.herokuapp.com/',
@@ -25,14 +33,13 @@ const projects = [
         git: 'https://github.com/Abel-Zambrano/cat-database'
     },
     {
-        id: 4,
-        img: 'https://live.staticflickr.com/65535/50546368751_3981d6859e_b.jpg',
-        name: 'Pokemon Card',
-        url: 'https://tarty-paint.surge.sh/',
-        tech: 'React, HTML5, CSS3, JavaScript, AJAX, APIs',
-        git: 'https://github.com/Abel-Zambrano/pokemon-api'
+        id: 5,
+        img: 'https://live.staticflickr.com/65535/50391646853_f348ed9fdb_o.png',
+        name: 'Airbnb Magic Clone Page',
+        url: 'https://airbnb-magic.surge.sh/',
+        tech: 'HTML5, CSS3',
+        git: 'https://github.com/Abel-Zambrano/airbnb'
     }
-
 ]
 
 export default projects;
