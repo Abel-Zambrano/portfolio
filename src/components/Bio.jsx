2ed3d3a53191040c33c3bfbbfb63fc1f517e948a
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-05 17:19:25 -0800

    update to bio

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -7,9 +7,9 @@ function Bio() {
                 <img src="https://live.staticflickr.com/65535/50373707011_c89ca24e84.jpg" alt="Abel Zambrano"/>
             </div>
             <div className='bio-content'>
-                <h1>ABEL ZAMBRANO | FULL STACK WEB DEVELOPER</h1>
-                <p>Graduated from California State University, East Bay with a B.S. in Business - <strong>Information Technology Management.</strong></p>
-                <p>I have experience building web applications consisting of responsive and dynamic design. I enjoy turning ideas into functioning applications. Love taking on challenging tasks and learning new technologies.</p>
+                <h1>ABEL ZAMBRANO | WEB DEVELOPER</h1>
+                <p>Graduated from California State University - East Bay with a B.S. in <strong>Information Technology Management.</strong></p>
+                <p>I have experience building web applications consisting of responsive and dynamic design. I enjoy turning ideas into functioning applications and taking on challenging tasks.</p>
             </div>
         </div>
     );
